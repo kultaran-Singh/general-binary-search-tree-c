f1aac2d530cbfd32c71b8b467aa4f65633ce70b6
Kultaran <kultaran99@proton.me>
2025-10-20 23:20:51 +0530

    Created Node and Tree structures, declared the basic functions

--- src/bst.c
+++ src/bst.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "bst.h"
+
+
+typedef struct Node{
+    void* data;
+    Node* left;
+    Node* right;
+}Node;
+
+typedef struct Tree{
+    Node*  root;
+    size_t size;                                       //Number of nodes
+    void   (*freeNode)(void* Node);                    //Function to free a node
+    void   (*compareNode)(void* node1, void* node2);   //Function to compare two nodes
+}Tree;
+
+//Lifecycle Functions
+Tree* tree_create(void (*freeNode)(void* Node),
+                  void (*compareNode)(void* node1, void* node2)){
+
+    Tree* tree = (Tree*)malloc(sizeof(Tree*));
+
+    //Configuring tree
+    tree->root        = calloc(1, sizeof(Node*));
+    tree->size        = 0;
+    tree->freeNode    = freeNode;
+    tree->compareNode = compareNode;
+    
+    return  tree;
+    
+}
--- src/bst.h
+++ src/bst.h
@@ -1,4 +1,23 @@
 #ifndef BST_C
 #define BST_C
 
+typedef struct Node Node;
+typedef struct Tree Tree;
+
+//Lifecycle Functions
+Tree* tree_create(void (*freeNode)(void* Node),
+                  void (*compareNode)(void* node1, void* node2));
+
+void tree_destroy(Tree* tree);
+
+//Insertion
+void tree_insert(Tree* tree, void* data);
+
+//Deletion
+void tree_remove(Tree* tree, void* data);
+
+//Helper Functions
+void* get_data(Node* node); //Returns the data of the node
+void tree_print(Tree* tree); 
+
 #endif
