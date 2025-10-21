4d28ebed6f0c5575195aefcbdd2ceb36ece8b6c1
Kultaran <kultaran99@proton.me>
2025-10-21 16:41:41 +0530

    Added Inorder, Postorder, Preorder traversal functions

--- src/bst.c
+++ src/bst.c
@@ -14,12 +14,12 @@ typedef struct Tree{
     Node*  root;
     size_t size;                                       //Number of nodes
     void   (*freeNode)(void* Node);                    //Function to free a node
-    void   (*compareNode)(void* node1, void* node2);   //Function to compare two nodes
+    int    (*compareNode)(Node* node1, Node* node2);   //Function to compare two nodes
 }Tree;
 
 //Lifecycle Functions
-Tree* tree_create(void (*freeNode)(void* Node),
-                  void (*compareNode)(void* node1, void* node2)){
+Tree* bst_create(void (*freeNode)(void* Node),
+                 int (*compareNode)(Node* node1, Node* node2)){
 
     Tree* tree = (Tree*)malloc(sizeof(Tree*));
 
@@ -30,5 +30,160 @@ Tree* tree_create(void (*freeNode)(void* Node),
     tree->compareNode = compareNode;
     
     return  tree;
+}
+
+void bst_destroy(Tree* tree){
+    //This function should free all nodes and free the data inside the node, set both the pointers to NULL,
+    //then free the node, then free the tree and set the tree pointer to NULL
+}
+//Core Functions
+void bst_insert(Tree* tree, void* data){
     
+    //Creating a new node
+    Node* new_node = calloc(1, sizeof(Node*));
+    new_node->data = data;
+
+    //If the tree is empty
+    if(tree->root->data == NULL){
+        tree->root = new_node;
+        tree->root->left = NULL;
+        tree->root->right = NULL;
+        return;
+    }
+    bst_insert_rec(tree, tree->root, data);
+    return;
+}
+
+//Traversal Functions
+void bst_traverse_inorder(Tree* tree, void (*op)(void* data, void* context), void* context){
+    bst_traverse_inorder_rec(tree->root, op, context);
+    return;
+}
+
+void bst_traverse_preorder(Tree* tree, void (*op)(void* data, void* context), void* context){
+    bst_traverse_postorder_rec(tree->root, op, context);
+    return;
+}
+
+void bst_traverse_postorder(Tree* tree, void (*op)(void* data, void* context), void* context){
+    bst_traverse_postorder_rec(tree->root, op, context);
+    return;
+}
+
+//Helper Functions
+void* get_data(Node* node){
+    return node->data;
+}
+
+void bst_print(Tree* tree){
+    bst_traverse_inorder(tree, node_print_int, NULL);
+    return;
+}
+
+int bst_isBalanced(Tree* tree){
+
+}
+
+//Recursion Helpers
+void bst_traverse_inorder_rec(Node* current_node, void (*op)(void* data, void* context), void* context){
+
+    if(current_node == NULL || op == NULL)
+        return;
+    //Visit Left node
+    if(current_node->left != NULL){
+        bst_traverse_inorder_rec(current_node->left, op, context);
+    }
+    //Visit Root node
+    if(context != NULL)
+        op(current_node, context);
+    if(context == NULL)
+        op(current_node, NULL);
+    //Visit Right node
+    if(current_node->right != NULL){
+        bst_traverse_inorder_rec(current_node->right, op, context);
+    }
+    return;
+}
+
+void bst_traverse_preorder_rec(Node* current_node, void (*op)(void* data, void* context), void* context){
+
+    if(current_node == NULL || op == NULL)
+        return;
+    //Visit Root node
+    if(context != NULL)
+        op(current_node, context);
+    if(context == NULL)
+        op(current_node, NULL);
+    //Visit Left node
+    if(current_node->left != NULL){
+        bst_traverse_inorder_rec(current_node->left, op, context);
+    }
+    //Visit Right node
+    if(current_node->right != NULL){
+        bst_traverse_inorder_rec(current_node->right, op, context);
+    }
+    return;
+}
+
+void bst_traverse_postorder_rec(Node* current_node, void (*op)(void* data, void* context), void* context){
+
+    if(current_node == NULL || op == NULL)
+        return;
+    //Visit Right node
+    if(current_node->right != NULL){
+        bst_traverse_inorder_rec(current_node->right, op, context);
+    }
+    //Visit Left node
+    if(current_node->left != NULL){
+        bst_traverse_inorder_rec(current_node->left, op, context);
+    }
+    //Visit Root node
+    if(context != NULL)
+        op(current_node, context);
+    if(context == NULL)
+        op(current_node, NULL);
+    
+    return;
+}
+
+int bst_insert_rec(Tree* tree, Node* current_node, void* insertData){
+    
+    if(current_node->left == NULL && current_node->right == NULL){
+        result_current = tree->compareNode(insertData, current_node->data)
+    }
+    void* left_node_value    = current_node->left->data;
+    void* right_node_value   = current_node->right->data;
+
+    result_left  = tree->compareNode(insertData, left_node_value);
+    result_right = tree->compareNode(insertData, right_node_value);
+    //IF insertData is less than left node data but more than curent node data,
+    //IF insertData is more than current node data but less than right node data.
+
+    switch(result_left){
+        case 1: //If an equivalent node already exists, return 1
+            return 1;
+            break;
+        case 2: //If data is more than the data of left_node
+            break;
+        case 3: //If data is less than the data of left_node
+            bst_insert_rec(tree, current_node->left, insertData);
+            break;
+    }
+    switch(result_right){
+        case 1: //If an equivalent node already exists, return 1
+            return 1;
+            break;
+        case 2: //If data is more than the data of right_node
+            bst_inser_rec(tree, current_node->right, insertData);
+            break;
+        case 3: //If data is less than the data of right_node
+            break;
+    }
+    return 0;
+}
+//DEMO
+void node_print_int(void* node, void* context){
+    Node* n = (Node*)node;
+    int* node_data = (int*)n->data;
+    printf("DATA = %d\n", *node_data);
 }
--- src/bst.h
+++ src/bst.h
@@ -5,19 +5,52 @@ typedef struct Node Node;
 typedef struct Tree Tree;
 
 //Lifecycle Functions
-Tree* tree_create(void (*freeNode)(void* Node),
-                  void (*compareNode)(void* node1, void* node2));
+Tree* bst_create(void (*freeNode)(void* Node),
+                  int (*compareNode)(Node* node1, Node* node2));
 
-void tree_destroy(Tree* tree);
+void bst_destroy(Tree* tree);
 
-//Insertion
-void tree_insert(Tree* tree, void* data);
+//Core Functions
+void bst_insert(Tree* tree, void* data);
+void bst_remove(Tree* tree, void* data);
+void* bst_search(Tree* tree, void* data);
 
-//Deletion
-void tree_remove(Tree* tree, void* data);
+//Traversal Functions
+void bst_traverse_inorder(Tree* tree, 
+                          void (*op)(void* data, void* context), 
+                          void* context);
 
-//Helper Functions
+void bst_traverse_preorder(Tree* tree, 
+                           void (*op)(void* data, void* context), 
+                           void* context);
+
+void bst_traverse_postorder(Tree* tree,
+                            void (*op)(void* data, void* context),
+                            void* context);
+
+//Utility Functions
 void* get_data(Node* node); //Returns the data of the node
-void tree_print(Tree* tree); 
+size_t bst_size(Tree* tree); //Returns number of nodes
+void* bst_get_min(Tree* tree);
+void* bst_get_max(Tree* tree);
+int bst_get_height(Tree* tree); //Returns height of tree
+int bst_isBalanced(Tree* tree);
+void bst_print(Tree* tree);
+void node_print_int(void* node, void* context); 
+
+//Recursion Helpers
+void bst_traverse_inorder_rec(Node* current_node, 
+                              void (*op)(void* data, void* context), 
+                              void* context);
+
+void bst_traverse_preorder_rec(Node* current_node, 
+                               void (*op)(void* data, void* context), 
+                               void* context);
+
+void bst_traverse_postorder_rec(Node* current_node,
+                                void (*op)(void* data, void* context), 
+                                void* context);
+
+int bst_insert_rec(Node* current_node, void* data);
 
 #endif
